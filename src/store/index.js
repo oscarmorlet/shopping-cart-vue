ab1f62d128a00838ef3a410cbbe994793f1436c2
oscar <oscarmorlet@gmail.com>
2018-05-16 17:31:48 -0500

    Shopping cart terminado pero sin el uso de higher order functions (Advanced Vuex Options).

---
+++ src/store/index.js
@@ -0,0 +1,108 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import shop from '@/api/shop'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    products: [],
+    cart: [],
+    checkoutStatus: null
+  },
+
+  getters: {
+    availableProducts (state, getters) {
+      return state.products.filter(product => product.inventory > 0)
+    },
+
+    cartProducts (state) {
+      return state.cart.map(cartItem => {
+          const product = state.products.find(product => product.id === cartItem.id)
+          return {
+              title: product.title,
+              price: product.price,
+              quantity: cartItem.quantity
+          }
+      })
+
+    },
+
+    cartTotal (state, getters) {
+      return getters.cartProducts.reduce( (total, product) => total + product.price * product.quantity, 0 )
+    }
+  },
+
+  actions: {
+    fetchProducts ({commit}) {
+      return new Promise((resolve, reject) => {
+
+          shop.getProducts(products => {
+            commit('setProducts', products)
+            resolve()
+          })
+
+      });
+    },
+
+    addProductToCart (context, product) {
+
+      if (product.inventory > 0) {
+
+        const cartItem = context.state.cart.find(item => item.id === product.id)
+
+        if (!cartItem) {
+          context.commit('pushProductToCart', product.id)
+        } else {
+          context.commit('incrementItemQuantity', cartItem)
+        }
+
+        context.commit('decrementProductInventory', product)
+      }
+    },
+
+    checkout ({state, commit}) {
+      shop.buyProducts(
+          state.cart,
+          () => {
+            commit('emptyCart')
+            commit('setCheckoutStatus', 'success')
+          },
+          () => {
+            commit('setCheckoutStatus', 'fail')
+          }
+      )
+    }
+  },
+
+  mutations: {
+    setProducts (state, products) {
+      //update products
+      state.products = products
+    },
+    //const cartItem = {id:123, quantity: 2}
+    pushProductToCart (state, productId) {
+      state.cart.push({
+        id: productId,
+        quantity: 1
+      })
+    },
+
+    incrementItemQuantity (state, cartItem) {
+        cartItem.quantity++
+    },
+
+    decrementProductInventory(state, product) {
+        product.inventory--
+    },
+
+    setCheckoutStatus (state, status) {
+      state.checkoutStatus = status
+    },
+
+    emptyCart (state) {
+      state.cart = []
+    }
+  }
+
+})
